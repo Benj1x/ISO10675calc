d9ab3aed8900ac070ac4e02ca2f8daaa6340f2d7
Benjamin Høj <44369126+Benj1x@users.noreply.github.com>
2024-09-08 11:51:20 +0200

    Pushing new page, missing code for doing math

---
+++ js/calc.js
@@ -0,0 +1,17 @@
+document.getElementById("Back").addEventListener("click", home);
+function home(){
+  window.location.href="index.html"
+}
+
+document.getElementById("ZValue").addEventListener(on)
+
+function CalculateAToZ(){
+    z = document.getElementById("ZValue").value;
+    return cos45 * z;
+}
+
+
+function CalculateAToZ(){
+    a = document.getElementById("AValue").value;
+    return (2*a).sqrt();
+}
