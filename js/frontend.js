2a2aec5704ab2e9520a11c203ade25b6b8bf5e27
Benj1x <benjaminh2001@hotmail.dk>
2024-09-25 22:28:15 +0200

    Update frontend.js

--- js/frontend.js
+++ js/frontend.js
@@ -12,9 +12,13 @@ function Navigate(){
 localStorage.clear();
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/js/service-worker.js')
-    .then(() => { console.log('Service Worker Registered'); });
+  navigator.serviceWorker.register('/service-worker.js')
+  .then(registration => {
+      console.log('Service Worker registered with scope:', registration.scope);
+  })
+  .catch(error => {
+      console.log('Service Worker registration failed:', error);
+  });
 }
 
 
