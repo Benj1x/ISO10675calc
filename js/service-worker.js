aab3f6dc48b65f572c125fa37092bdc56cb4c163
Benj1x <benjaminh2001@hotmail.dk>
2024-09-25 22:26:46 +0200

    updated sw

--- js/service-worker.js
+++ js/service-worker.js
@@ -3,18 +3,17 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('IsoCalcCache').then(cache => {
             return cache.addAll([
-                '/',
-                '/index.html',
-                '/results.html',
-                '/index.css',
-                '/js/frontend.js',
-                '/js/results.js',
-                '/js/GradeB.js',
-                '/js/GradeC.js',
-                '/js/GradeD.js',
-                '/imgs/logo.png',
-                '/imgs/LinkedInLogo.png',
-                '/imgs/githubLogo.png'
+                '../index.html',
+                '../results.html',
+                '../index.css',
+                '../js/frontend.js',
+                '../js/results.js',
+                '../js/GradeB.js',
+                '../js/GradeC.js',
+                '../js/GradeD.js',
+                '../imgs/logo.png',
+                '../imgs/LinkedInLogo.png',
+                '../imgs/githubLogo.png'
             ]);
         })
     );
